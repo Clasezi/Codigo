03fc8b9e759651b732a3a41c8aa5add68ef807e1
Clasezi <92495520+Clasezi@users.noreply.github.com>
2021-12-20 21:05:32 -0800

    Add files via upload

--- Ejercicio01/hanoi.py
+++ Ejercicio01/hanoi.py
@@ -16,6 +16,11 @@ def Llenar_torreFinalconInicial():
     disco_retirar = TorreInicial.pop()
     TorreFinal.items.append(disco_retirar)
 
+def Llenar_torreFinal_Medio():
+    disco_retirar = TorreMedio.pop()
+    TorreFinal.items.append(disco_retirar)
+    
+
 def Llenar_TorreMedioconInicial():
     disco_retirar = TorreInicial.pop()
     TorreMedio.items.append(disco_retirar)
@@ -28,15 +33,22 @@ def Llenar_TorreInicialconFinal():
     disco_retirar = TorreFinal.pop()
     TorreInicial.items.append(disco_retirar)
 
+def Llenar_TorreInicialconMedio():
+    disco_retirar = TorreMedio.pop()
+    TorreInicial.items.append(disco_retirar) 
+    
 
 
 if cantidad == 3: 
     Llenar_torreInicial()
-    print("La torre incial tiene : ", TorreInicial.items)
+    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_torreFinalconInicial()
-    print("La torre inicial tiene: ",TorreInicial.items)
-    print("La torre final tiene: ", TorreFinal.items)
+    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
     Llenar_TorreMedioconInicial()
-    print("La torre Medio tiene: ",TorreMedio.items)
-    print("La torre inicial tiene: ", TorreInicial.items)
-    
+    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    Llenar_TorreMedioconFinal()
+    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    Llenar_torreFinalconInicial()
+    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
+    Llenar_TorreInicialconMedio()
+    print("Las torre contienen los siguientes discos: ", TorreInicial.items, TorreMedio.items, TorreFinal.items)
